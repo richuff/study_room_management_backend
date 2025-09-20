8f4d76d028d29edc5a771c2eb3fe682ba2060e22
richuff <1691401076@qq.com>
2025-09-20 20:12:52 +0800

    feat:init project

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/spf13/viper"
+	"study_room_management_backend/router"
+	"study_room_management_backend/utils"
+)
+
+func main() {
+	utils.InitConfig()
+	utils.InitMysql()
+
+	r := router.Router()
+	err := r.Run(viper.GetString("server.port"))
+	if err != nil {
+		return
+	}
+}
